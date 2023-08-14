686c717c836d4f529c52657ae8f5fd649311b58c
Valentinas <31697821+valentk777@users.noreply.github.com>
2023-08-14 12:47:38 +0300

    expand metro

--- metro.config.js
+++ metro.config.js
@@ -1,30 +1,34 @@
-/**
- * Metro configuration for React Native
- * https://github.com/facebook/react-native
- *
- * @format
- */
+// /**
+//  * Metro configuration for React Native
+//  * https://github.com/facebook/react-native
+//  *
+//  * @format
+//  */
 
-module.exports = {
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: true,
-      },
-    }),
-  },
+// module.exports = {
+//   transformer: {
+//     getTransformOptions: async () => ({
+//       transform: {
+//         experimentalImportSupport: false,
+//         inlineRequires: true,
+//       },
+//     }),
+//   },
+// };
+
+
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+
+/**
+ * Metro configuration
+ * https://facebook.github.io/metro/docs/configuration
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
+const config = {
+    resolver: {
+        sourceExts: ['png', 'jpeg', 'jpg'],
+    },
 };
 
-
-// const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
-
-// /**
-//  * Metro configuration
-//  * https://facebook.github.io/metro/docs/configuration
-//  *
-//  * @type {import('metro-config').MetroConfig}
-//  */
-// const config = {};
-
-// module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
