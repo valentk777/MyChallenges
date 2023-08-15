5d848e99fde735627e388bb8a136977dbc06db22
Valentinas <31697821+valentk777@users.noreply.github.com>
2023-08-15 10:05:30 +0300

    fix pipeline

--- android/app/src/main/java/com/baitupasaulis/challengetracker/MainApplication.java
+++ android/app/src/main/java/com/baitupasaulis/challengetracker/MainApplication.java
@@ -1,6 +1,7 @@
 package com.baitupasaulis.challengetracker;
 
 import android.app.Application;
+import com.facebook.react.BuildConfig;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
