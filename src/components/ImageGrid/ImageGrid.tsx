ecdcb945848a874c2e952bf07e4915959e71c212
Valentinas <31697821+valentk777@users.noreply.github.com>
2023-10-05 09:32:09 +0300

    select icons from list

---
+++ src/components/ImageGrid/ImageGrid.tsx
@@ -0,0 +1,90 @@
+import React, { useContext } from 'react';
+import { Modal, View, StyleSheet, TouchableOpacity, FlatList } from 'react-native';
+import { customTheme } from '../../styles/customTheme';
+import { ThemeContext } from '../../contexts/themeContext';
+import LinearGradient from 'react-native-linear-gradient'
+
+interface ChallengesListProps {
+  visible: boolean;
+  imagesToDisplay: any[];
+  onClose: (imageLocation: string) => void;
+  currentIndex: number;
+}
+
+const ImageGalleryModal = ({ visible, imagesToDisplay, onClose, currentIndex }: ChallengesListProps) => {
+  const { theme } = useContext(ThemeContext);
+  const styles = createStyles(theme);
+
+  const imagesToDisplayLenght = imagesToDisplay.length;
+
+  const currentIndexLocation = imagesToDisplay[currentIndex % imagesToDisplayLenght].location;
+
+  const renderItem = ({ item }) => {
+    const Component = item.component;
+
+    return (
+      <TouchableOpacity onPress={() => onClose(item.location)} style={item.location == currentIndexLocation ? styles.selectedImage : styles.image}>
+        <Component/>
+      </TouchableOpacity>
+    )
+  };
+
+  return (
+    <Modal visible={visible} animationType="slide">
+      <LinearGradient
+        colors={styles.linearGradient.colors}
+        style={styles.linearGradient}
+      >
+        <View style={styles.container}>
+          <View style={styles.modalContent}>
+            <FlatList
+              data={imagesToDisplay}
+              renderItem={renderItem}
+              keyExtractor={(item) => item.location}
+              numColumns={3}
+              showsVerticalScrollIndicator={false}
+              showsHorizontalScrollIndicator={false}
+            />
+          </View>
+        </View>
+      </LinearGradient>
+    </Modal>
+  );
+};
+
+const createStyles = (theme: typeof customTheme) => {
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    linearGradient: {
+      flex: 15,
+      colors: [theme.colors.primary, theme.colors.secondary]
+    },
+    modalContent: {
+      width: '80%',
+      height: '90%',
+      backgroundColor: theme.colors.white,
+      borderRadius: 10,
+      overflow: 'hidden',
+    },
+    image: {
+      width: '33.3%',
+      aspectRatio: 1,
+      resizeMode: 'contain',
+    },
+    selectedImage: {
+      width: '33.3%',
+      aspectRatio: 1,
+      resizeMode: 'contain',
+      backgroundColor: 'blue'
+      // ...theme.shadows.dark
+    },
+  });
+
+  return styles;
+};
+
+export default ImageGalleryModal;
--- src/components/ImageSwapper/ImageSwapper.tsx
+++ src/components/ImageSwapper/ImageSwapper.tsx
@@ -3,15 +3,19 @@ import { View, StyleSheet, FlatList, TouchableOpacity } from 'react-native';
 import { SvgComponents } from '../../assets/svgIndex';
 import { customTheme } from '../../styles/customTheme';
 import { ThemeContext } from '../../contexts/themeContext';
+import ImageGalleryModal from '../ImageGrid/ImageGrid';
 
 const ImageSwapper = ({ onImageChange }) => {
     const { theme } = useContext(ThemeContext);
     const styles = createStyles(theme);
 
-    const [containerWidth, setContainerWidth] = useState(0);
-    const currentIndex = 50;
-    const flatListRef = useRef(null);
+
     const svgComponentsLenth = SvgComponents.length;
+    const flatListRef = useRef(null);
+
+    const [containerWidth, setContainerWidth] = useState(0);
+    const [currentIndex, setCurrentIndex] = useState(svgComponentsLenth * 2);
+    const [modalVisible, setModalVisible] = useState(false);
 
     const onLayout = event => {
         // return current object layout, not the screen
@@ -19,6 +23,10 @@ const ImageSwapper = ({ onImageChange }) => {
 
         setContainerWidth(width);
     };
+
+    const getSvgFileName = (index: number) => {
+        return SvgComponents[index % svgComponentsLenth].location;
+    }
 
     const getItemLayout = (_, index) => ({
         length: containerWidth,
@@ -31,6 +39,7 @@ const ImageSwapper = ({ onImageChange }) => {
 
         return (
             <TouchableOpacity
+            onPress={onPressIcon}
                 activeOpacity={0.9}
                 style={{ ...styles.imageContainer, width: containerWidth }}
             >
@@ -42,16 +51,31 @@ const ImageSwapper = ({ onImageChange }) => {
     const handleMomentumScrollEnd = useCallback(event => {
         const contentOffset = event.nativeEvent.contentOffset.x;
         const visibleIndex = Math.floor((contentOffset + containerWidth / 2) / containerWidth);
-        const currentComponentFileName = SvgComponents[visibleIndex % svgComponentsLenth].location;
+        const currentComponentFileName = getSvgFileName(visibleIndex);
 
+        setCurrentIndex(visibleIndex);
         onImageChange(currentComponentFileName);
     }, [containerWidth]);
 
+    const handleOnClose = (imageLocation) => {
+        const index = SvgComponents.findIndex((item) => item.location === imageLocation) + 2 * svgComponentsLenth;
+
+        onImageChange(imageLocation);
+        setCurrentIndex(index);
+        setModalVisible(false);
+    }
+
+    const onPressIcon = () => {
+        setModalVisible(true);
+    }
+
     return (
         <View style={styles.container} onLayout={onLayout}>
+            <ImageGalleryModal visible={modalVisible} imagesToDisplay={SvgComponents} onClose={handleOnClose} currentIndex={currentIndex} />
             <FlatList
+                key={currentIndex}
                 ref={flatListRef}
-                data={Array.from({ length: 100 })}
+                data={Array.from({ length: svgComponentsLenth * 4 })}
                 renderItem={({ index }) => renderItem({ index })}
                 horizontal
                 pagingEnabled
