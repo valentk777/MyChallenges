6c0fba9fc724794bf08afa94332b400d2126a809
Valentinas <31697821+valentk777@users.noreply.github.com>
2023-10-06 20:44:28 +0300

    rename project

--- ios/MyChallengesTests/MyChallengesTests.m
+++ ios/MyChallengesTests/MyChallengesTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface ChallengeTrackerTests : XCTestCase
+@interface MyChallengesTests : XCTestCase
 
 @end
 
-@implementation ChallengeTrackerTests
+@implementation MyChallengesTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
